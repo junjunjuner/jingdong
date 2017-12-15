f143f6aa315ad31bc4f11fac4bfa45c5f3d8be0d
260141 <523614801@qq.com>
2017-12-15 11:44:01 +0800

    京东电烤箱代码

---
+++ jd_diankaoxiang/修改后jddkx_price2 2017.12.7/jditems/items.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class JdItem(scrapy.Item):
+    #商品型号
+    p_Name = scrapy.Field()
+    #店铺名称
+    shop_name = scrapy.Field()
+    #商品ID
+    ProductID = scrapy.Field()
+    #正价
+    price = scrapy.Field()
+    #折扣价
+    PreferentialPrice = scrapy.Field()
+    #评论总数
+    CommentCount = scrapy.Field()
+    #好评度
+    GoodRateShow = scrapy.Field()
+    #好评
+    GoodCount = scrapy.Field()
+    #中评
+    GeneralCount = scrapy.Field()
+    #差评
+    PoorCount = scrapy.Field()
+    #评论关键字
+    keyword = scrapy.Field()
+    #类别
+    type = scrapy.Field()
+    #品牌
+    brand = scrapy.Field()
+    #外观样式
+    X_type = scrapy.Field()
+    #商品型号
+    X_name = scrapy.Field()
+    #控制方式
+    control=scrapy.Field()
+    # 温控方式
+    temp_con=scrapy.Field()
+    # 适用人数
+    people=scrapy.Field()
+    # 特色功能
+    function=scrapy.Field()
+    #产品尺寸
+    size=scrapy.Field()
+    #开门方式
+    open_type=scrapy.Field()
+    #容量
+    capacity=scrapy.Field()
+    #颜色
+    color=scrapy.Field()
+    product_url=scrapy.Field()
+    #来源
+    source=scrapy.Field()
+    # 爬取时间
+    ProgramStarttime=scrapy.Field()
+
+
---
+++ jd_diankaoxiang/修改后jddkx_price2 2017.12.7/jditems/settings.py
@@ -0,0 +1,119 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for jdtest project
+#
+# For simplicity, this file contains only settings considered important or
+# commonly used. You can find more settings consulting the documentation:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#     http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
+#     http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
+
+import time
+import random
+
+BOT_NAME = 'jditems'
+
+SPIDER_MODULES = ['jditems.spiders']
+NEWSPIDER_MODULE = 'jditems.spiders'
+
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+jd_user_agent=[
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36"
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1"
+    "Mozilla/5.0 (X11; CrOS i686 2268.111.0) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1092.0 Safari/536.6",
+    "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1090.0 Safari/536.6",
+    "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/19.77.34.5 Safari/537.1",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.9 Safari/536.5",
+    "Mozilla/5.0 (Windows NT 6.0) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.36 Safari/536.5",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+    "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_0) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+    "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
+    "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+    "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+    "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.0 Safari/536.3",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24",
+    "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24"
+]
+
+# Obey robots.txt rules
+ROBOTSTXT_OBEY = False
+
+# Configure maximum concurrent requests performed by Scrapy (default: 16)
+#CONCURRENT_REQUESTS = 32
+RETRY_ENABLED=True
+RETRY_TIMES=5                #重试次数
+# Configure a delay for requests for the same website (default: 0)
+# See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
+# See also autothrottle settings and docs
+DOWNLOAD_DELAY =2                 #下载延迟
+# The download delay setting will honor only one of:
+CONCURRENT_REQUESTS_PER_DOMAIN = 16                #并发数
+
+# Disable cookies (enabled by default)
+COOKIES_ENABLED = False                    #关闭cookies
+
+DOWNLOADER_MIDDLEWARES = {
+#    'gome.middlewares.MyCustomDownloaderMiddleware': 543,
+    'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
+    'jditems.middlewares.JdUseragentMiddleware': 400,
+    'jditems.middlewares.Exceptions': 300
+}
+
+ITEM_PIPELINES = {
+   'jditems.pipelines.JdtestPipeline': 300,
+    'jditems.pipelines.CSVPipeline': 200
+
+}
+
+#电烤箱字段
+FIELDS_TO_EXPORT = [
+    'p_Name',
+    'shop_name',
+    'ProductID',
+    'price',
+    'PreferentialPrice',
+    'CommentCount',
+    'GoodRateShow',
+    'GoodCount',
+    'GeneralCount',
+    'PoorCount',
+    'keyword',
+    'type',         #核心参数
+    'brand',        #品牌
+    'X_name',       #型号
+    'control',      #控制方式
+    'X_type',       #外观样式
+    'capacity',     #容量
+    'temp_con',     #温控方式
+    'people',       #适用人数
+    'function',     #特色功能
+    'color',        #颜色
+    'size',         #产品尺寸
+    'open_type',    #开门方式
+    'product_url',
+    'source',
+    'ProgramStarttime'
+]
+
+# AUTOTHROTTLE_ENABLED = True                    #开启自动限速算法
+# The initial download delay
+AUTOTHROTTLE_START_DELAY = 3
+# The maximum download delay to be set in case of high latencies
+AUTOTHROTTLE_MAX_DELAY = 60
+
+# AUTOTHROTTLE_DEBUG = True                   #可看到是如何进行自动限速的
+
+MONGO_HOST = "172.28.171.13"  # 主机IP
+MONGO_PORT = 27017  # 端口号
+MONGO_DB = "DKX"  # 库名
+MONGO_COLL = "dkx_jd"  # 文档(相当于关系型数据库的表名)
+# LOG_FILE='jd电烤箱_log_%s.txt' % (time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()))
+LOG_FILE='jddkx_log.txt'
+RANDOMIZE_DOWNLOAD_DELAY=True
+
